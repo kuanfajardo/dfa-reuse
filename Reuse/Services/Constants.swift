f35802121e7e59c881bcf2b82dad499a4a5932f4
Juan Diego Fajardo <jfajardo@mit.edu>
2018-03-07 12:23:27 -0500

    Add empty files for app-wide services and ColorTheme resource. Create new directory structure.

---
+++ Reuse/Services/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Reuse
+//
+//  Created by Juan Diego Fajardo on 3/7/18.
+//  Copyright © 2018 Design for America - MIT. All rights reserved.
+//
+
+import Foundation
---
+++ Reuse/Services/Errors.swift
@@ -0,0 +1,9 @@
+//
+//  Errors.swift
+//  Reuse
+//
+//  Created by Juan Diego Fajardo on 3/7/18.
+//  Copyright © 2018 Design for America - MIT. All rights reserved.
+//
+
+import Foundation
